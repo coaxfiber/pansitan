9df5e1680fd73411ffc8cf2ce9d60b3ad31d81b0
coaxfiber <coaxfiber05@gmail.com>
2019-03-09 12:02:59 +0800

    g

---
+++ src/app/app.component.ts
@@ -0,0 +1,121 @@
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+
+import {PropertyListPage} from '../pages/property-list/property-list';
+//import {BrokerListPage} from '../pages/broker-list/broker-list';
+//import {FavoriteListPage} from '../pages/favorite-list/favorite-list';
+import {WelcomePage} from '../pages/welcome/welcome';
+import {AboutPage} from '../pages/about/about';
+import { AccountPage } from '../pages/account/account';
+import {AddstayinnPage} from '../pages/addstayinn/addstayinn';
+//import { AlertController } from 'ionic-angular';
+
+import { GlobalvarsProvider } from '../providers/globalvars/globalvars';
+import { AdMobFree, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
+ 
+export interface MenuItem {
+    title: string;
+    component: any;
+    icon: string;
+}
+
+
+@Component({
+    templateUrl: 'app.html'
+})
+export class MyApp {
+    @ViewChild(Nav) nav: Nav;
+
+    rootPage: any = WelcomePage;
+
+    appMenuItems: Array<MenuItem>;
+
+    accountMenuItems: Array<MenuItem>;
+
+    helpMenuItems: Array<MenuItem>;
+
+    constructor( public admob: AdMobFree,public platform: Platform,public globalvars: GlobalvarsProvider, public statusBar: StatusBar, public splashScreen: SplashScreen/*,private alertCtrl: AlertController*/) {
+        this.initializeApp();
+         this.launchInterstitial();
+        //this.presentConfirm();
+        this.appMenuItems = [
+            {title: 'Stay Inns', component: PropertyListPage, icon: 'home'},
+            {title: 'Add Stay Inns', component: AddstayinnPage, icon: 'md-add-circle'},
+            //{title: 'Brokers', component: BrokerListPage, icon: 'people'},
+            //{title: 'Favorites', component: FavoriteListPage, icon: 'star'},
+            //{title: 'Get Preapproved', component: WelcomePage, icon: 'checkmark-circle'},
+        ];
+
+        this.accountMenuItems = [
+            {title: 'Account', component: AccountPage, icon: 'ios-contact'},
+           // {title: 'Logout', component: WelcomePage, icon: 'log-out'},
+        ];
+
+        this.helpMenuItems = [
+            {title: 'Featured', component: WelcomePage, icon: 'bookmark'},
+            {title: 'About', component: AboutPage, icon: 'information-circle'},
+        ];
+
+
+
+
+
+    }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            this.statusBar.styleLightContent();
+            this.splashScreen.hide();
+        });
+    }
+
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.setRoot(page.component);
+    }
+
+
+    launchInterstitial() {
+ 
+        let interstitialConfig: AdMobFreeInterstitialConfig = {
+            autoShow: true,
+            id: 'ca-app-pub-1127440969430875/8807385000'
+            //id: Your Ad Unit ID goes here
+        };
+ 
+        this.admob.interstitial.config(interstitialConfig);
+ 
+        this.admob.interstitial.prepare().then(() => {
+            // success
+        });
+ 
+    }
+   /* presentConfirm() {
+          let alert = this.alertCtrl.create({
+            title: 'Fb Login',
+            message: 'Stay inn Locator requires you to login to your fb account.',
+            buttons: [
+              {
+                text: 'Close App',
+                role: 'cancel',
+                handler: () => {
+                  this.platform.exitApp();
+                }
+              },
+              {
+                text: 'Agree',
+                handler: () => {
+                    
+                }
+              }
+            ]
+          });
+          alert.present();
+       }*/
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,79 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import {WelcomePage} from '../pages/welcome/welcome';
+import {PropertyListPage} from '../pages/property-list/property-list';
+import {PropertyDetailPage} from '../pages/property-detail/property-detail';
+import {BrokerListPage} from '../pages/broker-list/broker-list';
+import {BrokerDetailPage} from '../pages/broker-detail/broker-detail';
+import {FavoriteListPage} from '../pages/favorite-list/favorite-list';
+import {AboutPage} from '../pages/about/about';
+import {AccountPage} from '../pages/account/account';
+import {AddstayinnPage} from '../pages/addstayinn/addstayinn';
+
+import {PropertyService} from "../providers/property-service-mock";
+import {BrokerService} from "../providers/broker-service-mock";
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { FacebookModule } from 'ngx-facebook';
+import { Facebook } from '@ionic-native/facebook';
+import { GlobalvarsProvider } from '../providers/globalvars/globalvars';
+import { Camera } from '@ionic-native/camera';
+import { Transfer } from '@ionic-native/transfer';
+import { File } from '@ionic-native/file';
+import { FilePath } from '@ionic-native/file-path';
+
+import { AdMobFree } from '@ionic-native/admob-free';
+@NgModule({
+  declarations: [
+    MyApp,
+    WelcomePage,
+    AboutPage,
+    PropertyListPage,
+    PropertyDetailPage,
+    FavoriteListPage,
+    BrokerListPage,
+    BrokerDetailPage,
+    AddstayinnPage,
+    AccountPage
+
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    FacebookModule.forRoot(),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    WelcomePage,
+    AboutPage,
+    AddstayinnPage,
+    PropertyListPage,
+    PropertyDetailPage,
+    FavoriteListPage,
+    BrokerListPage,
+    BrokerDetailPage,
+    AccountPage,
+  ],
+  providers: [ 
+    Transfer,
+    File,
+    Camera,
+    AdMobFree,
+    FilePath,
+    StatusBar,
+    SplashScreen,
+    PropertyService,
+    BrokerService,SplashScreen, StatusBar, Facebook,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, GlobalvarsProvider,
+    
+  ]
+})
+export class AppModule {}
+  
